5716b8b34b592c459151b862107bc4399a601f16
unknown <mconwa02@dcs.bbk.ac.uk>
2020-04-18 22:58:23 +0100

    advertising data csv and plots

---
+++ data_analysis/advertising_data.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+import os
+
+DATA_DIR = r"C:\project_data\ISLR"
+
+df = pd.read_csv(os.path.join(DATA_DIR, "Advertising.csv"), )
+df = df.iloc[:, 1:]
+print(df)
+print(df.info())
+print(df.describe())
+
+print(df.TV.plot())
