24a24fb654f947e1cecdd62fe78074e21fd44683
unknown <mconwa02@dcs.bbk.ac.uk>
2020-07-19 17:21:10 +0100

    adding boston data names

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
 DATA_DIR = r"C:\project_data\ISLR"
+
+BOSTON_NAMES = {
+    "CRIM": "crime_rate",
+    "ZN": "residential_land_over_25000_sq_ft",
+    "INDUS": "non_retail_business",
+    "CHAS": "suburbs_close_to_charles_river",
+    "NOX": "nitric_oxides_concentration",
+    "RM": "avg_number_of_rooms",
+    "AGE": "owner_occupied_units_1940",
+    "DIS": "distances_to_employment_centres",
+    "RAD": "accessibility_to_highways",
+    "TAX": "property_tax_rate",
+    "PTRATIO": "pupil_teacher_ratio",
+    "B": "black_ethnicity",
+    "LSTAT": "population_lower_status",
+    "MEDV": "owner_occupied_homes_value",
+}
