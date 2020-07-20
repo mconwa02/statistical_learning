3e360f0de4ec5f4c814eb1561201ef2ae73529d4
unknown <mconwa02@dcs.bbk.ac.uk>
2020-07-20 22:55:20 +0100

    moving files to utilies

--- data_analysis/advertising_data.py
+++ data_analysis/advertising_data.py
@@ -1,7 +1,7 @@
 import os
 
 import pandas as pd
-from config import DATA_DIR
+from utilities.config import DATA_DIR
 
 df = pd.read_csv(os.path.join(DATA_DIR, "Advertising.csv"), )
 df = df.iloc[:, 1:]
